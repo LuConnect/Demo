3816986e61e8bdc6904464489ae6cd27e9c869e6
nafim <cse_2012020194@lus.ac.bd>
2023-02-02 13:18:07 +0600

    3rd

--- app/src/main/java/com/example/demo/MainActivity2.java
+++ app/src/main/java/com/example/demo/MainActivity2.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +11,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity2 extends AppCompatActivity {
 
     EditText suser,sid,spass1,semail,spass2;
     TextView signup;
+    String emailReg = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,9 @@ public class MainActivity2 extends AppCompatActivity {
         semail = findViewById(R.id.email);
         spass2 = findViewById(R.id.pass2);
         signup = findViewById(R.id.signup);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
 
 
         //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
@@ -33,62 +49,71 @@ public class MainActivity2 extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user= suser.getText().toString();
-                String id= sid.getText().toString();
-                String pass1= spass1.getText().toString();
-                String email= semail.getText().toString();
-                String pass2= spass2.getText().toString();
 
-
-                if (user.length()>0){
-                    if(id.length()>0){
-                        if(email.length()>0){
-
-                            if(pass1.length()>0){
-                                if(pass2.length()>0){
-                                    Toast.makeText(getApplicationContext(),"Registration Successfull!",Toast.LENGTH_SHORT).show();
-
-                                    startActivity(new Intent(MainActivity2.this,MainActivity3.class));
-                                }
-                                else{
-                                    spass2.setError("Password");
-                                }
-
-                            }
-                            else {
-                                spass1.setError("Password");
-                            }
-                        }
-                        else {
-                            semail.setError("Email!");
-                        }
-
-                    }
-                    else{
-                        sid.setError("Student id");
-
-                    }
-                }
-                else {
-                    suser.setError("Input Name");
-                    sid.setError("Student id");
-                    semail.setError("Email!");
-                    spass1.setError("Password");
-                    spass2.setError("Password");
-                }
-
+                performAuth();
 
             }
 
-
-
-
-
-
         });
 
+    }
+
+    private void performAuth() {
+
+        String user= suser.getText().toString();
+        String id= sid.getText().toString();
+        String pass1= spass1.getText().toString();
+        String email= semail.getText().toString();
+        String pass2= spass2.getText().toString();
+
+        if(user.length()<0)
+        {
+            suser.setError("invalid username");
+        }
+        else if(id.length()>11 && id.length()<9){
+            sid.setError("invalid student id");
+        }
+        else if(!email.matches(emailReg))
+        {
+            semail.setError("Enter correct email");
+        }else if(pass1.isEmpty() || pass1.length()<8)
+        {
+            spass1.setError("Enter minimum 8 length of password");
+        }
+        else if(!pass1.equals(pass2)){
+            spass2.setError("password not matched");
+        }
+        else{
+            progressDialog.setMessage("please wait while registration is completing...");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,pass1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        sendusertologin();
+                        Toast.makeText(MainActivity2.this, "registration successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity2.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
 
 
 
+    }
+
+    private void sendusertologin() {
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
