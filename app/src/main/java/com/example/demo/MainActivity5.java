f41bdd1a63a77098ec3b3e923815b42f907b0196
Mafruda <mafrudajulfa1000@gmail.com>
2023-02-04 23:11:28 +0600

    activity5

--- app/src/main/java/com/example/demo/MainActivity5.java
+++ app/src/main/java/com/example/demo/MainActivity5.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity5 extends AppCompatActivity {
 
-    Button b1;
+    TextView b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
